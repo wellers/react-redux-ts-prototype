37a20024336ea3e96d6bafc6387b689e5971ce4a
wellers <paul_welbourne@yahoo.co.uk>
2023-01-25 19:35:12 +0000

    Clean-up unnecessary JS files in Client project.

--- Client/app/types/contacts.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REQUEST_CONTACTS = 'REQUEST_CONTACTS';
-exports.RECEIVE_CONTACTS = 'RECEIVE_CONTACTS';
-exports.RECEIVE_SERVER_ERROR = 'RECEIVE_SERVER_ERROR';
-//# sourceMappingURL=types.js.map
